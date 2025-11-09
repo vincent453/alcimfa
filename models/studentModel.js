1c5c29d9478f8885e0986390fbe282854f30d08e
lawrence vincent <93725993+vincent453@users.noreply.github.com>
2025-11-09 21:43:30 +0100

    Update studentModel.js

--- models/studentModel.js
+++ models/studentModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const studentSchema = new mongoose.Schema(
   {
@@ -48,9 +49,71 @@ const studentSchema = new mongoose.Schema(
       trim: true,
       lowercase: true,
     },
+    // PIN for student login
+    pin: {
+      type: String,
+      select: false, // Don't return PIN by default in queries
+      minlength: 4,
+    },
+    // Track if PIN has been set
+    hasPinSet: {
+      type: Boolean,
+      default: false,
+    },
+    // Track last PIN change
+    pinLastChanged: {
+      type: Date,
+    },
   },
   { timestamps: true }
 );
+
+// Hash PIN before saving
+studentSchema.pre("save", async function (next) {
+  // Only hash if PIN is modified
+  if (!this.isModified("pin")) {
+    return next();
+  }
+  
+  // If PIN is being set or changed
+  if (this.pin) {
+    const salt = await bcrypt.genSalt(10);
+    this.pin = await bcrypt.hash(this.pin, salt);
+    this.hasPinSet = true;
+    this.pinLastChanged = new Date();
+  }
+  
+  next();
+});
+
+// Method to compare entered PIN with hashed PIN
+studentSchema.methods.comparePin = async function (enteredPin) {
+  if (!this.pin) {
+    return false;
+  }
+  return await bcrypt.compare(enteredPin, this.pin);
+};
+
+// Method to generate a random PIN
+studentSchema.methods.generatePin = function () {
+  // Generate 6-digit PIN
+  return Math.floor(100000 + Math.random() * 900000).toString();
+};
+
+// Static method to generate PIN for multiple students
+studentSchema.statics.generatePinForStudent = async function (studentId) {
+  const student = await this.findById(studentId);
+  if (!student) {
+    throw new Error("Student not found");
+  }
+  
+  // Generate 6-digit PIN
+  const pin = Math.floor(100000 + Math.random() * 900000).toString();
+  student.pin = pin;
+  await student.save();
+  
+  return pin; // Return unhashed PIN to send to student/parent
+};
 
 const Student = mongoose.model("Student", studentSchema);
 export default Student;
